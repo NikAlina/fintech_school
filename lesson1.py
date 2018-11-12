d837dde62855c443809b4fa1764a0dfc704eb6a2
Alina <A.Shebeleva@mail.ru>
2018-11-12 22:26:27 +0700

    lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,4 @@
 import requests
 headers = {'Accept':'application/vnd.github.v3+json'}
-response = requests.get('https://api.github.com/users/NikAlina', headers=headers)
+response = requests.get('https://api.github.com/users/NikAlina',headers=headers)
 print(response.text)
