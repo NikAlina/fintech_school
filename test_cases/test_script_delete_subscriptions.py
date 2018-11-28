44cc8663143345a43917c8343251a0b1ef040930
Alina <A.Shebeleva@mail.ru>
2018-11-28 22:54:13 +0700

    lesson 6

--- test_cases/test_script_delete_subscriptions.py
+++ test_cases/test_script_delete_subscriptions.py
@@ -8,6 +8,6 @@ def test_script(http_client):
     2.Check response status code is 200.
     """
 
-    response = http_client.del_subscriptions(request_id='555', system_code='222')
+    response = http_client.del_subscriptions (request_id='555', system_code='222')
 
     assert response.status_code == 200
