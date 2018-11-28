6d08951229ebcc08b5d1bb2e92b3a9f23d8b5d8a
Alina <A.Shebeleva@mail.ru>
2018-11-28 22:57:56 +0700

    lesson 6

--- packages/http_client/http_client.py
+++ packages/http_client/http_client.py
@@ -31,7 +31,7 @@ class HttpClient(object):
     def post_subscriptions(self, request_id, system_code):
 
 
-        url_post_subscriptions = self.base_url + 'contacts/a.kitaeva/subscriptions?request_id=' + request_id + '&system_code=' + system_code
+        url_post_subscriptions = self.base_url + 'contacts/A.kitaeva/subscriptions?request_id=' + request_id + '&system_code=' + system_code
 
         response = requests.post(url_post_subscriptions, json.dumps({"instrument_id":"AAPL_SPBXM","sec_name":"AAPL","sec_type":"equity","price_alert":0}), headers=headers)
 
@@ -39,7 +39,7 @@ class HttpClient(object):
 
     def del_subscriptions(self, request_id, system_code):
         headers = {'Authorization': 'Basic ZmludGVjaDoxcTJ3M2Uh'}
-        url_del_subscriptions = self.base_url + 'contacts/a.kitaeva/subscriptions/0x039D7730?request_id=' + request_id + '&system_code=' + system_code
+        url_del_subscriptions = self.base_url + 'contacts/A.kitaeva/subscriptions/0x039D7730?request_id=' + request_id + '&system_code=' + system_code
 
         response = requests.delete(url_del_subscriptions, headers=self.headers)
 
@@ -48,7 +48,7 @@ class HttpClient(object):
     def get_instruments_details(self, request_id, system_code):
 
 
-        url_del_subscriptions = self.base_url + 'contacts/a.kitaeva/subscriptions/0x039D7730?request_id=' + request_id + '&system_code=' + system_code
+        url_del_subscriptions = self.base_url + 'contacts/A.kitaeva/subscriptions/0x039D7730?request_id=' + request_id + '&system_code=' + system_code
 
         response = requests.delete(url_del_subscriptions, headers=self.headers)
 
